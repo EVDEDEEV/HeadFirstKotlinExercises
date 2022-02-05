89e53911f5f15d910f0cae61bca745d6617407b2
evdedeev <dedeeverkesh@gmail.com>
2022-02-05 22:39:11 +0300

    Add Map Collection code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+data class Recipe(var name: String)
+
 fun main() {
     var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
     println("mShoppingList original: $mShoppingList")
@@ -11,7 +13,7 @@ fun main() {
     println("mShoppingLust sorted:$mShoppingList")
     mShoppingList.reverse()
     println("mShoppingList reversed: $mShoppingList")
-
+//Set collection code
     val mShoppingSet = mShoppingList.toMutableSet()
     println("mShoppingSet: $mShoppingSet")
     val moreShopping = setOf("Chives", "Spinach", "Milk")
@@ -19,4 +21,21 @@ fun main() {
     println("mShoppingSet items added: $mShoppingSet")
     mShoppingList = mShoppingSet.toMutableList()
     println("mShoppingList new version: $mShoppingList")
+//Map Collection code
+    val r1 = Recipe("Chicken Soup")
+    val r2 = Recipe("Quinoa Salad")
+    val r3 = Recipe("Thai Curry")
+    val r4 = Recipe("Jambalaya")
+    val r5 = Recipe("Sausage Rolls")
+    val mRecipeMap = mutableMapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
+    println("mRecipeMap original: $mRecipeMap")
+    val recipesToAdd = mapOf("Recipe4" to r4, "Recipe5" to r5)
+    mRecipeMap.putAll(recipesToAdd)
+    println("mRecipeMap updated: $mRecipeMap")
+    if (mRecipeMap.containsKey("Recipe1")) {
+        println("Recipe1 is: ${mRecipeMap.getValue("Recipe1")}")
+    }
+
+
+
 }
